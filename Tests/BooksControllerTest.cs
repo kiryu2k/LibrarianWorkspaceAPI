1e43c9cd7a397f9beb4ea94c1be32c8c50885ec1
Kirill Ryabov <sailor.kirill@gmail.com>
2023-11-15 13:19:34 +0700

    Add basic add book test

--- Tests/BooksControllerTest.cs
+++ Tests/BooksControllerTest.cs
@@ -6,7 +6,11 @@ using Moq;
 using Moq.EntityFrameworkCore;
 using NuGet.Protocol;
 using System.Text.Json.Serialization;
+using LibrarianWorkspace.Dto;
 using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,34 +21,62 @@ namespace LibrarianWorkspace.Tests;
 
 public class BooksControllerTest
 {
+    private List<Book> _data;
+    private Mock<DbSet<Book>> _setMock;
+    private Mock<LibrarianWorkspaceContext> _ctxMock;
     private readonly ITestOutputHelper _testOutputHelper;
-
+    
+    // public BooksControllerTest()
+    // {
+    //     _data = GetFakeBookList();
+    //     _setMock = _data.AsQueryable().BuildMockDbSet();
+    //     _ctxMock = new Mock<LibrarianWorkspaceContext>();
+    //     _setMock.Setup(x => x.FindAsync(_data[0].Id)).ReturnsAsync(_data[0]);
+    //     _setMock.Setup(x => x.AddAsync(It.IsAny<Book>(),It.IsAny<CancellationToken>())).Callback((Book model, CancellationToken token)=> {_data.Add(model);});
+    //     _ctxMock.Setup(x => x.Book).Returns(_setMock.Object);
+    // }
+    
     public BooksControllerTest(ITestOutputHelper testOutputHelper)
     {
+        _data = GetFakeBookList();
+        _setMock = _data.AsQueryable().BuildMockDbSet();
+        _ctxMock = new Mock<LibrarianWorkspaceContext>();
+        _setMock.Setup(x => x.FindAsync(_data[0].Id)).ReturnsAsync(_data[0]);
+        _setMock.Setup(x => x.AddAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback((Book model, CancellationToken token) => { _data.Add(model); });
+        _ctxMock.Setup(x => x.Book).Returns(_setMock.Object);
         _testOutputHelper = testOutputHelper;
     }
 
     [Fact]
     public async void GetBook()
     {
-        var stub = GetFakeBookList();
-        var data = stub.AsQueryable().BuildMockDbSet();
-        var ctxMock = new Mock<LibrarianWorkspaceContext>();
-        // var setMock = new Mock<DbSet<Book>>();
-        // setMock.As<IQueryable<Book>>().Setup(x=>x.Provider).Returns(data.Provider);
-        // setMock.As<IQueryable<Book>>().Setup(x=>x.ElementType).Returns(data.ElementType);
-        // setMock.As<IQueryable<Book>>().Setup(x=>x.Expression).Returns(data.Expression);
-        // setMock.As<IQueryable<Book>>().Setup(x=>x.GetEnumerator()).Returns(()=>data.GetEnumerator());
-        // ctxMock.Setup(x => x.Book).Returns(setMock.Object);
-        data.Setup(x => x.FindAsync(stub[0].Id)).ReturnsAsync(stub[0]);
-        ctxMock.Setup(x => x.Book).Returns(data.Object);
-        var controller = new BooksController(ctxMock.Object);
+        var controller = new BooksController(_ctxMock.Object);
         const int bookId = 1;
         var jsonData = (await controller.GetBook(bookId)).ToJson();
         Assert.NotNull(jsonData);
         var resp = JObject.Parse(jsonData!);
         var book = JsonConvert.DeserializeObject<Book>(resp.GetValue("Value")!.ToString());
-        Assert.Equivalent(stub[0], book);
+        Assert.Equivalent(_data[0], book);
+    }
+
+    [Fact]
+    public async void CreateBook()
+    {
+        var controller = new BooksController(_ctxMock.Object);
+        var dto = new BookDto()
+        {
+            Name = "added book",
+            Article = "AAAAAA",
+            Author = "meowich",
+            PublicationYear = 2017,
+            Quantity = 0
+        };
+        await controller.CreateBook(dto);
+        const int expBookId = 3;
+        var jsonData = ( await controller.CreateBook(dto)).ToJson();
+        Assert.NotNull(jsonData);
+        Assert.Equal(expBookId, _data.Count);
     }
     
     private static List<Book> GetFakeBookList()
